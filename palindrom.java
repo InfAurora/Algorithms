657ce192441b30e583bed68ae5d04364f7c4141d
InfAurora <albajo0123@gmail.com>
2021-04-13 01:09:40 -0500

    Palindrome set up/start of idea

--- palindrom.java
+++ palindrom.java
@@ -1,10 +1,18 @@
 public class palindrom {
   public static void main(String[] args) {
-    
+    String wordOne = "racecar";
+    String wordTwo = "helloworld";
+    String wordThree = "A dog! A panic in a pagoda.";
+    System.out.println(reverseWord(wordOne));
+    System.out.println(reverseWord(wordTwo));
+    System.out.println(reverseWord(wordThree));
   }
 
-  public static String reverseWord() {
-
+  public static String reverseWord(String wordToBeReversed) {
+    String onlyLetters = wordToBeReversed.replaceAll("[^a-zA-Z]", "");
+    for (int i = 0; i < onlyLetters.length(); i++) {
+      
+    }
     return null;
   }
 }
