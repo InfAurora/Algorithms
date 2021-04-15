ef2ba1e6abc745ba90e6e3b829ccc928c951ec8e
InfAurora <albajo0123@gmail.com>
2021-04-14 23:03:17 -0500

    Start of reverseSentenceAlgorithm

---
+++ Easy/reverseSentence.java
@@ -0,0 +1,17 @@
+package Easy;
+
+public class reverseSentence {
+  public static void main(String[] args) {
+    String sentenceOne = "";
+    String sentenceTwo = "";
+    String sentenceThree = "";
+    System.out.println(reverseSentence(sentenceOne));
+    System.out.println(reverseSentence(sentenceTwo));
+    System.out.println(reverseSentence(sentenceThree));
+  }
+
+  public static String reverseSentence(String sentenceToBeReversed) {
+    
+    return null;
+  }
+}
