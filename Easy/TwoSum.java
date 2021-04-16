6ebbd33b555450857239d6c337c678482198b54e
InfAurora <albajo0123@gmail.com>
2021-04-16 00:49:22 -0500

    Finished TwoSum

--- Easy/TwoSum.java
+++ Easy/TwoSum.java
@@ -1,12 +1,30 @@
 package Easy;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class TwoSum {
   public static void main(String[] args) {
-    
+    int[] numbersOne = {1, 2, -1, -3, 3, -4, 5};
+    int[] numbersTwo = {1, 2, 5};
+    int[] numbersThree = {-1, -3, -4, -5};
+    System.out.println(Arrays.toString(findTargetSum(numbersOne, -7)));
+    System.out.println(Arrays.toString(findTargetSum(numbersTwo, 7)));
+    System.out.println(Arrays.toString(findTargetSum(numbersThree, -9)));
   }
 
-  public static int[] findTargetSum(int[] arrayOfIntegers) {
+  public static int[] findTargetSum(int[] arrayOfIntegers, int target) {
     int[] indicies = new int[2];
+    HashMap<Integer, Integer> numberNeeded = new HashMap();
+    for (int i = 0; i <= arrayOfIntegers.length - 1; i++) {
+      if (numberNeeded.containsKey(target - arrayOfIntegers[i])) {
+        indicies[0] = numberNeeded.get(target - arrayOfIntegers[i]);
+        indicies[1] = i;
+        return indicies;
+      }
+      numberNeeded.put(arrayOfIntegers[i], i);
+      numberNeeded.put(target - arrayOfIntegers[i], i);
+    }
     return indicies;
   }
 }
