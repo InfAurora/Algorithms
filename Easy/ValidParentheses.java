be498f05600c41af50b63d5bed060d68bf7e732d
InfAurora <albajo0123@gmail.com>
2021-04-17 00:50:33 -0500

    Completed ValidParentheses; Need to refactor and also look up a better solution

--- Easy/ValidParentheses.java
+++ Easy/ValidParentheses.java
@@ -1,14 +1,50 @@
 package Easy;
 
+import java.util.Stack;
+
 public class ValidParentheses {
   // Time = 
   // Space = 
   public static void main(String[] args) {
-    
+    String validStringOne = "()";
+    String validStringTwo = "({()})";
+    String validStringThree = "(){()()[]}[]";
+    String invalidStringOne = "([)])";
+    String invalidStringTwo = "(";
+    String invalidStringThree = "[[[[[())]]]]]";
+    System.out.println(isValid(validStringOne));
+    System.out.println(isValid(validStringTwo));
+    System.out.println(isValid(validStringThree));
+    System.out.println(isValid(invalidStringOne));
+    System.out.println(isValid(invalidStringTwo));
+    System.out.println(isValid(invalidStringThree));
   }
 
   public static boolean isValid(String stringOfBrackets) {
-    
-    return false;
+    Stack<Character> stackOfBrackets = new Stack<>();
+    for (int i = 0; i < stringOfBrackets.length(); i++) {
+      if (stringOfBrackets.length() == 1) {
+        return false;
+      }
+      if(stringOfBrackets.charAt(i) == ')') {
+        if (stackOfBrackets.peek() == '(') {
+          stackOfBrackets.pop();
+        }
+        else return false;
+      } else if (stringOfBrackets.charAt(i) == ']') {
+        if (stackOfBrackets.peek() == '[') {
+          stackOfBrackets.pop();
+        }
+        else return false;
+      } else if (stringOfBrackets.charAt(i) == '}') {
+        if (stackOfBrackets.peek() == '{') {
+          stackOfBrackets.pop();
+        }
+        else return false;
+      } else {
+        stackOfBrackets.push(stringOfBrackets.charAt(i));
+      }
+    }
+    return true;
   }
 }
