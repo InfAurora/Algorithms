8ca052efa5f4716218c3372c9460980f302e583d
InfAurora <albajo0123@gmail.com>
2021-04-16 21:35:29 -0500

    Start of ValidParentheses Algorithm

---
+++ Easy/ValidParentheses.java
@@ -0,0 +1,14 @@
+package Easy;
+
+public class ValidParentheses {
+  // Time = 
+  // Space = 
+  public static void main(String[] args) {
+    
+  }
+
+  public static boolean isValid(String stringOfBrackets) {
+    
+    return false;
+  }
+}
