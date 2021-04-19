048a792c76001eb59083e0f54ec30cc79f9147cf
InfAurora <albajo0123@gmail.com>
2021-04-18 20:30:02 -0500

    Start of LongestCommonPrefix algorithm

---
+++ Easy/LongestCommonPrefix.java
@@ -0,0 +1,15 @@
+public class LongestCommonPrefix {
+  public static void main(String[] args) {
+    String[] arrayOfStringsOne = { "flower","flow","flight" };
+    String[] arrayOfStringsTwo = { "dog","racecar","car" };
+    System.out.println(findLongestCommonPrefix(arrayOfStringsOne));
+    System.out.println(findLongestCommonPrefix(arrayOfStringsTwo));
+  }
+
+  public static String findLongestCommonPrefix(String[] strs) {
+    String commonPrefix = "";
+
+    
+    return commonPrefix;
+  }
+}
