5989e2888d1551424075fbde628dc28cc69bc4ec
InfAurora <albajo0123@gmail.com>
2021-04-20 23:19:18 -0500

    Added 3 test cases for RomanToInteger; Might use stack for this algorithm

--- Easy/RomanToInteger.java
+++ Easy/RomanToInteger.java
@@ -1,9 +1,14 @@
+import java.util.Stack;
+
 public class RomanToInteger {
   public static void main(String[] args) {
-    
+    String romanNumberOne = "XVIII";
+    String romanNumberTwo = "VX";
+    String romanNumberThree = "IIX";
   }
 
   public static int romanNumberConversion(String s) {
+    Stack<Character> romanStack = new Stack<>();
     
     return -1;
   }
