56583369c9af1914d2cec20d8abfbacd52aa9e79
InfAurora <albajo0123@gmail.com>
2021-04-24 02:10:48 -0500

    Finished DepthFirstSearch; watch video explanation of what v and e are for time and space

--- Easy/DepthFirstSearch.java
+++ Easy/DepthFirstSearch.java
@@ -1,9 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class DepthFirstSearch {
+  // time = O(v + e)
+  // space = O(v)
   public static void main(String[] args) {
     
   }
 
-  public static void depthFirstSearch() {
-    
+  static class Node {
+    String name;
+    List<Node> children = new ArrayList<Node>();
+
+    public Node(String name) {
+      this.name = name;
+    }
+
+    public List<String> depthFirstSearch(List<String> array) {
+      // Write your code here.
+			array.add(this.name);
+			for(int i = 0; i < children.size(); i++) {
+				children.get(i).depthFirstSearch(array);
+			}
+      return array;
+    }
+
+    public Node addChild(String name) {
+      Node child = new Node(name);
+      children.add(child);
+      return this;
+    }
   }
 }
