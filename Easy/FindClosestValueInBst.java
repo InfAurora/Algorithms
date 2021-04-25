7cac31f7a6f7d1bca97ce8385357e36c5489e4d1
InfAurora <albajo0123@gmail.com>
2021-04-25 00:55:17 -0500

    Finished FindClosestValueInBst; look alternate solution

--- Easy/FindClosestValueInBst.java
+++ Easy/FindClosestValueInBst.java
@@ -1,3 +1,36 @@
 public class FindClosestValueInBst {
-  
+
+  // Average TIME = O(log(n)) | SPACE O(log(n))
+  // Worst TIME =  O(n) | SPACE O(n)
+  public static void main(String[] args) {
+    
+  }
+
+  public static int findClosestValueInBst(BST tree, int target) {
+    return findClosestValueInBst(tree, target, tree.value);
+  }
+	
+	public static int findClosestValueInBst(BST tree, int target, int closest) {
+		int currentNodeValue = tree.value;
+   	if (Math.abs(target - closest) > Math.abs(target - currentNodeValue)) {
+			closest = tree.value;
+		}
+		if (target < currentNodeValue && tree.left != null) {
+			return findClosestValueInBst(tree.left, target, closest);
+		} else if (target > currentNodeValue && tree.right != null) {
+			return findClosestValueInBst(tree.right, target, closest);
+		} else {
+			return closest;
+		}
+  }
+
+  static class BST {
+    public int value;
+    public BST left;
+    public BST right;
+
+    public BST(int value) {
+      this.value = value;
+    }
+  }
 }
