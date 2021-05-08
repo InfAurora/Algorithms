14820359ffe08371050c4a51b19d3578d5dcb58b
InfAurora <albajo0123@gmail.com>
2021-05-07 23:11:35 -0800

    First attempt at MoveElementToEnd Algorithm

---
+++ Medium/MoveElementToEnd.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MoveElementToEnd {
+  public static void main(String[] args) {
+    List<Integer> array = new ArrayList<>();
+    array.add(2);
+    array.add(1);
+    array.add(2);
+    array.add(2);
+    array.add(2);
+    array.add(3);
+    array.add(4);
+    array.add(2);
+    // {2, 1, 2, 2, 2, 3, 4, 2};
+    System.out.println(moveElementToEnd(array, 2));
+  }
+  public static List<Integer> moveElementToEnd(List<Integer> array, int toMove) {
+    // Write your code here.
+    for (int i = 0; i < array.size(); i++) {
+      System.out.println(array.indexOf(i));
+      if (array.get(i) == toMove) {
+        array.remove(i);
+        array.add(toMove);
+      }
+    }
+    return array;
+  }
+}
