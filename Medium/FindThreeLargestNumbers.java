fc3a36685517be599aab02d67e8a9670006153c9
InfAurora <albajo0123@gmail.com>
2021-05-09 01:45:11 -0500

    Started/Finished FindThreeLargestNumber Algorithm

---
+++ Medium/FindThreeLargestNumbers.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+public class FindThreeLargestNumbers {
+
+  public static void main(String[] args) {
+    int[] array = { 141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7 };
+    System.out.println(Arrays.toString(findThreeLargestNumbers(array)));
+  }
+  // Time = O(n)
+	// Space = O(1)
+  public static int[] findThreeLargestNumbers(int[] array) {
+    // Write your code here.
+		int biggest = Integer.MIN_VALUE;
+		int middle = Integer.MIN_VALUE;
+		int last = Integer.MIN_VALUE;
+		int[] biggestNumbers = {Integer.MIN_VALUE, Integer.MIN_VALUE, Integer.MIN_VALUE};
+		for (int number: array) {
+			if (number > biggestNumbers[2]) {
+				shiftBiggest(biggestNumbers, number);
+			} else if (number > biggestNumbers[1]) {
+				shiftMiddle(biggestNumbers, number);
+			} else if (number > biggestNumbers[0]) {
+				biggestNumbers[0] = number;
+			}
+		}
+    return biggestNumbers;
+  }
+
+	public static void shiftBiggest(int[] array, int biggest) {
+		int newMiddle = array[2];
+		int newSmallest = array[1];
+		array[2] = biggest;
+		array[1] = newMiddle;
+		array[0] = newSmallest;
+	}
+
+	public static void shiftMiddle(int[] array, int middle) {
+		int newSmallest = array[1];
+		array[1] = middle;
+		array[0] = newSmallest;
+	}
+}
