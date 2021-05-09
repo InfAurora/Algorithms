bf385019d9f8b3ce2f3551805d7b92322045a2fb
InfAurora <albajo0123@gmail.com>
2021-05-08 22:02:27 -0500

    Finished MoveElementToEnd that covers all cases

--- Medium/MoveElementToEnd.java
+++ Medium/MoveElementToEnd.java
@@ -3,6 +3,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MoveElementToEnd {
+  // 	Time = O(n)
+  // 	Space = O(1)
   public static void main(String[] args) {
     List<Integer> array = new ArrayList<>();
     array.add(2);
@@ -17,15 +19,15 @@ public class MoveElementToEnd {
     System.out.println(moveElementToEnd(array, 2));
   }
   public static List<Integer> moveElementToEnd(List<Integer> array, int toMove) {
-    // Write your code here.
-    // Need to shift keep pointer on same index position as number just removed
-    // Use seperate counter as stopping point
-    for (int i = 0; i < array.size(); i++) {
+    int count = 0;
+    for (int i = 0; count < array.size(); i++) {
       System.out.println(array.indexOf(i));
       if (array.get(i) == toMove) {
         array.remove(i);
         array.add(toMove);
+        i = i - 1;
       }
+      count++;
     }
     return array;
   }
