266a1605d22f55aa48f827929f54504f8baabd1d
InfAurora <albajo0123@gmail.com>
2021-05-12 01:54:05 -0500

    Start of ReconstructBst Algorithm

---
+++ Medium/ReconstructBst.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class ReconstructBst {
+  static class BST {
+    public int value;
+    public BST left = null;
+    public BST right = null;
+
+    public BST(int value) {
+      this.value = value;
+    }
+  }
+
+  public BST reconstructBst(ArrayList<Integer> preOrderTraversalValues) {
+    // Write your code here.
+    return null;
+  }
+}
