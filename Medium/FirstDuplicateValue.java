a7e051c074a3a6adb4ead291b87962c227d6e703
InfAurora <albajo0123@gmail.com>
2021-05-21 23:27:12 -0500

    Theory of how to complete firstDuplicateValue Algo

--- Medium/FirstDuplicateValue.java
+++ Medium/FirstDuplicateValue.java
@@ -1,6 +1,8 @@
 public class FirstDuplicateValue {
   public int firstDuplicateValue(int[] array) {
     // Write your code here.
+    // Use map to store past numbers and index position
+    // If number exists and has smallest index, then return it;
     return -1;
   }
 }
